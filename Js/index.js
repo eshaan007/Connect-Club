67c0bebd99fda83a3c172355db94009884cb4757
Kushang Shah <kushangshah131@gmail.com>
2020-05-25 12:00:25 +0530

    Including sliding tabs

---
+++ Js/index.js
@@ -0,0 +1,12 @@
+	function OpenLogIn()
+	{
+		document.getElementById("layer2").style.display = "none";
+		document.getElementById("layer1").style.display = "block";
+		
+	}
+	
+	function OpenSignIn()
+	{
+		document.getElementById("layer2").style.display = "block";
+		document.getElementById("layer1").style.display = "none";
+	}
