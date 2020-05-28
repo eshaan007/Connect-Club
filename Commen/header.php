e9de0ab8fb0c8d57e96273dc06cdb78c7c4c2ee0
Kushang Shah <kushangshah131@gmail.com>
2020-05-28 16:46:48 +0530

    Script and headers

--- Commen/header.php
+++ Commen/header.php
@@ -8,3 +8,23 @@
 <link rel="stylesheet" href="../CSS/kel.css">
 <style>
 </style>
+<script>
+    function myFunction() {
+      var x = document.getElementById("Demo");
+      if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+      } else { 
+        x.className = x.className.replace(" w3-show", "");
+      }
+    }
+</script>
+</head>
+<body>
+
+<header class="w3-bar w3-<?php echo $theme_color ?> w3-padding">
+<div class="w3-bar-item w3-xlarge w3-hide-small" style="margin-right:40px;">ConnectClub</div>
+<div class="w3-bar-item w3-large kel-hover-2" title="Home screen" style="margin-top:7px;margin-bottom:7px;">
+    <a href="https://keltagoodlife.co/Connect-Club/Users/profile?name=<?php echo $_REQUEST['name'] ?>" style="text-decoration:none">
+    <i class="fa fa-home"></i> Lobby
+    </a>
+</div>
