0f0307ac227e34c2204058b561a093b59c2fc8a0
Kushang Shah <kushangshah131@gmail.com>
2020-05-29 09:12:18 +0530

    update email varification
    To change the email in not varified email account

--- Users/Js/nonvarified.js
+++ Users/Js/nonvarified.js
@@ -63,3 +63,44 @@ class Check{
 				
 	}		
 }
+
+let update = () => {
+	    
+	    let email = document.getElementById('email').value;
+	    let check_data = new Check();
+	    let error = document.getElementById('error');
+	        
+	    if(email == ""){
+	        alert("Please enter your email");
+	        error.innerHTML = "Please enter your email";
+	        return false;
+	    }
+	    
+	    if(check_data.check(email)){
+	        
+	        return false;
+	    }
+	    
+	    if(!check_data.emailCheck(email, error)){
+	        return false;
+	    }
+	
+	let str = "Email="+email;
+        let xhttp = new XMLHttpRequest();
+        let loader = document.getElementById('loader');
+        xhttp.onreadystatechange = function() {
+        	loader.style.display = "block";
+        	if(this.readyState == 4 && this.status == 200){
+        		error.innerHTML = this.responseText;
+        		loader.style.display = "none";
+        		if(this.responseText == ""){
+        		    alert("Email updated and verification mail sent");
+        		    location.reload(); 
+         		}
+        	}
+        }
+        xhttp.open("POST", "updateEmail", true);
+        xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        xhttp.send(str);
+	    
+}
