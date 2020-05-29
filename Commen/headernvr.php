369ca287fb8f78cdaa566d299631d20fac4ecb39
Kushang Shah <kushangshah131@gmail.com>
2020-05-29 11:30:02 +0530

    Body and other header parts

--- Commen/headernvr.php
+++ Commen/headernvr.php
@@ -8,3 +8,31 @@
 <link rel="stylesheet" href="../CSS/kel.css">
 <style>
 </style>
+<body>
+<?php
+    $uri = $_SERVER['REQUEST_URI'];
+    $protocol = ((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
+    $url = $protocol . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+?>
+
+<header class="w3-bar w3-green w3-padding">
+<div class="w3-bar-item w3-xlarge w3-hide-small" style="margin-right:40px;">ConnectClub</div>
+<div class="w3-bar-item w3-large kel-hover w3-hover-white" title="Home screen" style="margin-top:7px;margin-bottom:7px;">
+    <a href="<?php echo $url ?>" style="text-decoration:none"><i class="fa fa-home"></i> Lobby
+    </a>
+</div>
+<button class="w3-bar-item w3-right w3-border kel-hover w3-hover-white" title="My Account" style="margin-top:7px;margin-bottom:7px" onclick="document.getElementById('editun').style.display='block'">
+    <i class="fa fa-lock"></i> Kushang
+</button>
+
+<div class="w3-dropdown-click w3-bar-item w3-right w3-green w3-hover-green">
+<button class="kel-hover-2 w3-button w3-green w3-hover-green" onclick="myFunction()" title="Options" >
+    <i class="fa fa-caret-down"></i>
+</button>
+    <div id="Demo" class="w3-dropdown-content w3-bar-block w3-border">
+      <a onclick="document.getElementById('editun').style.display='block'" class="w3-bar-item w3-button"><i class="fa fa-pencil"></i> Edit</a>
+      <a href="../logout" class="w3-bar-item w3-button"><i class="fa fa-sign-out"></i> LogOut</a>
+    </div>
+</div>
+
+</header>
