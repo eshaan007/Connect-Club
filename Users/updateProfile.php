950070ddae0d92430c72b6e6d1c7aa0789d6ef07
Kushang Shah <kushangshah131@gmail.com>
2020-05-30 07:50:54 +0530

    updateProfile

---
+++ Users/updateProfile.php
@@ -0,0 +1,86 @@
+<?php
+
+    session_start();
+	function test_input($data) 
+	{
+		$data = trim($data);
+		$data = stripslashes($data);
+		$data = htmlspecialchars($data);
+		return $data;
+	}
+
+	if(isset($_SESSION['login_user_connect_club']) && isset($_REQUEST['u_name']) && isset($_REQUEST['cur_pass']) && isset($_REQUEST['newPass']) && isset($_REQUEST['newAga'])){
+		
+		require_once("../../../Database/dbconnect_chat.php");
+        $dts = explode("&",base64_decode($_SESSION['login_user_connect_club']));
+        $id = $dts[0];
+        $name = $dts[2];
+        
+		$u_name = test_input($_REQUEST['u_name']);
+		$Pass = test_input($_REQUEST['cur_pass']);
+		$new_pass = test_input($_REQUEST['newPass']);
+		$new_pass_again = test_input($_REQUEST['newagainPass']);
+		
+		require_once("../../../Database/dbconnect_chat.php");
+        
+        $opt = [
+			'cost' => 12
+		];
+
+        $query = "SELECT u_name FROM users WHERE u_name = '$u_name'";
+		
+		if($data = $conn->query($query)){
+		    
+		    if($data->num_rows > 0){
+		        die("This username is already taken");
+		    }
+		    
+		}
+		else{
+		    die("Something went wrong");
+		}
+		
+
+		$query = "SELECT u_password FROM users WHERE u_id = $u_id";
+		if($data = $conn->query($query)){
+			
+			if($data->num_rows<=0){
+				echo "Password didn't match";
+			}
+			else{
+				$result = $data->fetch_assoc();
+					
+				$dbpass = $result['u_password'];
+					
+				if(password_verify($Pass, $dbpass)){
+						
+					$pass = password_hash($new_pass, PASSWORD_BCRYPT, $opt);
+					
+					$query1 = "UPDATE users SET u_password = '$pass', u_name = '$u_name' WHERE u_id = $u_id";
+					if($conn->query($query1)){
+						
+					}
+					else{
+						echo "Password didn't match";
+					}
+				}
+				else{
+					echo "Password didn't match";
+				}
+				
+			}
+			
+		}
+		else{
+			echo "Something went wrong";
+		}
+		
+	}
+	else if(isset($_SESSION['login_user_connect_club'])){
+		echo "Login first";
+	}
+	else{
+		header('Location:logout');
+	}
+	
+?>
