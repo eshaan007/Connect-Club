7a5b05825862ab5b7481ca58c27ed7cf01c43b8f
Kushang Shah <kushangshah131@gmail.com>
2020-05-31 23:41:41 +0530

    Proper nonvarified user's profile page

--- Users/Parts/nonverifiedprofile.php
+++ Users/Parts/nonverifiedprofile.php
@@ -32,6 +32,39 @@
     }
 ?>
         <hr>
-         </form>
+        <button type="button" class="w3-button w3-right w3-margin-bottom w3-margin-left w3-border w3-round w3-green" onclick="alert('please verify your email')"><i class="fa fa-pencil-square-o"></i> Update</button>
+        <button type="button" onclick="document.getElementById('editun').style.display='none'" class="w3-button w3-right w3-margin-bottom w3-border w3-round"><i class="fa fa-times"></i> Cancel</button>
+        
+      </form>
+      
+    </div>
 </div>
 <center>
+<div class='w3-content'>
+    <div class="w3-padding w3-large">
+       Check your mailbox to find our verification mail...
+    </div>
+    <hr>
+    <div class="w3-padding">
+        We have sent an email to your registered email address. If you are unable to find it then please check in spam mails. 
+        <br><b>If still you are unable to find the mail then you can again ask for another varification mail. Fill the below form to ask for a new 
+        varification mail.</b>
+    </div>
+    <form>
+        <center>
+			<div class="w3-text-red" id="error"></div>
+			<div class="loader" id="loader" style="display:none"></div>
+		</center>
+        <div class="w3-section">
+            Email:<input type="text" class="w3-input w3-border" placeholder="name@email.com" style="max-width:450px;" id="email">
+        </div>
+        <div class="w3-section">
+            <input type="button" class="w3-button w3-green" value="Send" onclick="update()">
+        </div>
+    </form>
+</div>
+</center>
+
+<script src="Js/nonvarified.js"></script>
+</body>
+</html>
