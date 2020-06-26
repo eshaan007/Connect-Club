ad4e568a4427627ca7fae5fc51b597024b195f11
eshaan007 <42370985+eshaan007@users.noreply.github.com>
2020-06-26 21:27:53 +0530

    Added dorecovery page

---
+++ dorecovery.php
@@ -0,0 +1,41 @@
+<?php
+function test_input($data) 
+{
+	$data = trim($data);
+	$data = stripslashes($data);
+	$data = htmlspecialchars($data);
+	return $data;
+}
+	
+if(isset($_REQUEST['data']) && isset($_REQUEST['pass'])){
+
+	$datas = base64_decode($_REQUEST['data']);
+	$datas = explode("&", $datas);
+	$email = $datas[1];
+	$pass = $_REQUEST['pass'];
+	
+	$opt = [
+      'cost' => 12,  
+    ];
+            
+    $pass = password_hash($pass, PASSWORD_BCRYPT, $opt);
+    $query = "UPDATE users SET u_password = '$pass' WHERE email = '$email'";
+	require_once("../../Database/dbconnect_chat.php");
+    
+	if($conn->query($query)){
+	    
+	}
+	else{
+	    echo "Something went wrong";
+	}
+	
+	
+?>
+
+<?php
+}
+else{
+     header("Location:../logout"); 
+}
+
+?>
