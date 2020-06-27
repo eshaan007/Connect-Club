b836d0574429af6c5ff3a9ba402a556cbccdff71
Kushang Shah <kushangshah131@gmail.com>
2020-06-27 07:00:17 +0530

    Update people.php
    People table with the visit and poke button.

--- Users/people.php
+++ Users/people.php
@@ -85,3 +85,151 @@ if(isset($_SESSION['login_user_connect_club']) && isset($_REQUEST['name'])){
 <?php
     }
     else{
+        echo "something went wrong";
+    }
+?>
+        <hr>
+        <button type="button" class="w3-button w3-right w3-margin-bottom w3-margin-left w3-border w3-round w3-<?php echo $theme_color ?>" onclick="updateProfile()"><i class="fa fa-pencil-square-o"></i> Update</button>
+        <button type="button" onclick="document.getElementById('editun').style.display='none'" class="w3-button w3-right w3-margin-bottom w3-border w3-round"><i class="fa fa-times"></i> Cancel</button>
+        
+      </form>
+      
+    </div>
+</div>
+
+<div class="w3-padding-64">
+<div class="w3-content w3-light-gray w3-padding-32">
+    <div class="w3-xxlarge w3-center w3-padding">Connected people</div>
+    
+    <hr style="border-bottom:1px solid black;" class="w3-margin">
+<?php
+    $query1 = "SELECT chat_clubs.club_id, club_name FROM(chat_club_member INNER JOIN chat_clubs ON chat_club_member.club_id = chat_clubs.club_id) WHERE u_id = $id";
+    if($data1 = $conn->query($query1)){
+        
+        if($data1->num_rows <= 0){
+            echo "<div class='w3-center w3-padding w3-margin'>You haven't connected to any people yet</div>";
+        }
+        else{
+        while($result1 = $data1->fetch_assoc()){
+            
+?>          
+<div class="w3-padding">
+<table class="w3-table w3-bordered w3-border">
+    <thead>
+        <tr class="w3-xlarge">
+            <td colspan="3" class="w3-center w3-<?php echo $theme_color ?>">People From: <b><?php echo $result1['club_name'] ?></b> Club</td>
+        </tr>
+        <tr class="w3-dark-gray w3-text-white">
+            <th>Name</th>
+            <th>Profile links</th>
+            <th>Operations</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+<?php
+    $club_id = $result1['club_id'];
+    $query2 = "SELECT otheruserview.u_id, u_name FROM (chat_club_member INNER JOIN otheruserview ON chat_club_member.u_id = otheruserview.u_id) WHERE club_id = $club_id";
+
+    if($data2 = $conn->query($query2)){
+        
+        if($data2->num_rows <= 0){
+            echo "<div class='w3-padding w3-margin w3-center'>There are no other user here</div>";
+        }
+        
+        while($result2 = $data2->fetch_assoc()){
+            
+            if($id == $result2['u_id']){
+                continue;
+            }
+?>
+
+        <tr>
+            <td><?php echo $result2['u_name'] ?></td>
+            <td>
+                <?php $url = base64_encode($id."&".$result2['u_name']."&".$result2['u_id']); ?>
+            <button class="w3-button kel-hover-2 w3-<?php echo $theme_color ?>" onclick="window.open('user?name=<?php echo $url ?>', '_self')">
+                Visit</button></td>
+            <td><button class="w3-button kel-hover-2 w3-<?php echo $theme_color ?>" 
+            onclick="poke(<?php echo $id ?>,<?php echo $result2['u_id'] ?>, '<?php echo $result2['u_name'] ?>')"><i class="fa fa-hand-o-right"></i>Poke</button></td>
+        </tr>
+<?php
+        }
+    }
+    else{
+        echo "Something went wrong";
+    }
+?>
+        </tr>
+    </tbody>
+</table>
+</div>
+<?php
+        }
+        }
+    }
+    else{
+        die("Something went wrong");
+    }
+    
+?>
+    
+    
+</div>
+</div>
+
+<script src="Js/varified.js"></script>
+<script src="../Js/check.js"></script>
+<script>
+
+let poke = (poker_id, recever_id, user = "someone") => {
+    
+    let str = "poker_id="+poker_id+"&recever_id="+recever_id;
+    let xhttp = new XMLHttpRequest();
+    
+    xhttp.onreadystatechange = function() {
+    	
+    	if(this.readyState == 4 && this.status == 200){
+    		
+    		if(this.responseText == ""){
+    		    alert("You just poked "+user);
+    		}
+    	}
+    }
+    xhttp.open("POST", "Action/poke", true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send(str);
+    
+}
+
+</script>
+
+</body>
+</html>
+<?php
+        }
+        else{
+           
+?>
+<!-- Notverified account area -->
+
+<?php
+            header("Location:../logout.php");
+        }
+        
+    }
+    else{
+        echo "Something went wrong, Please try again later"; 
+    }
+    
+    
+?>
+<?php
+    
+    $conn->close();
+}
+else{
+    header("Location:../logout"); 
+}
+
+?>
